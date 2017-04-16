ddb739f65dbf27fe5285d762b6f3e81fab72dc38
raineran16 <andreas.rainer2@edu.fh-joanneum.at>
2017-04-16 19:17:03 +0200

    added hundret; added todo

--- romanNumeral.js
+++ romanNumeral.js
@@ -1,11 +1,29 @@
 
 
 function calculateRomanNumeral(decimalValue, currentRomanValue) {
-    //1. M --- div 1000
+
+    //todo: 9 should result in IX, not VIIII; 4 should result in IV not IIII, ...
+    //todo: replace if ... else if with helper function
+    //http://www.mathsisfun.com/roman-numerals.html
+
     var divValue = 0;
     var modValue = 0;
 
-    if (decimalValue >= 50) {
+    if (decimalValue >= 100) {
+        divValue = Math.floor(decimalValue/100);
+        modValue = decimalValue % 100;
+
+        for(i=0;i<divValue;i++) {
+            currentRomanValue += "C";
+        }
+        if(modValue > 0) {
+            return calculateRomanNumeral(modValue,currentRomanValue);
+        }
+        else {
+            return currentRomanValue;
+        }
+    }
+    else if (decimalValue >= 50) {
         divValue = Math.floor(decimalValue/50);
         modValue = decimalValue % 50;
 
