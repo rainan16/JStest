6af5c95bb0c16dad21be7ec419e6d435920b3d59
raineran16 <andreas.rainer2@edu.fh-joanneum.at>
2017-04-18 20:15:40 +0200

    - description for whereforeArtThou - html cleanup - added "created by" in .html

--- whereforeArtThou.js
+++ whereforeArtThou.js
@@ -2,6 +2,13 @@
  * Created by raineran16 on 17.04.2017.
  */
 
+/* FUNCTION "whatIsInAName"
+ * Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument).
+ * Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+ * For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }],
+ * and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument),
+ * because it contains the property and its value, that was passed on as the second argument.
+ */
 function whatIsInAName(collection, source) {
     // What's in a name?
     var arr = [];
@@ -37,5 +44,5 @@ function whatIsInAName(collection, source) {
 function runWhatIsInAName() {
     var result = whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
     //2. test: result = whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
-    document.getElementById("resultText").innerHTML = "Result = " + result[Object.keys(result)[0]];
+    document.getElementById("resultText").innerHTML = "First result =>  [" + Object.keys(result[0])[0] + ": " + result[0][Object.keys(result[0])[0]] + "]";
 }
