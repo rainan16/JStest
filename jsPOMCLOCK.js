f8b54bd142e3c573da91121c69e1c5793b02d29f
raineran16 <andreas.rainer2@edu.fh-joanneum.at>
2017-04-26 21:06:30 +0200

    - added count down functionality and notification sound

--- jsPOMCLOCK.js
+++ jsPOMCLOCK.js
@@ -4,7 +4,7 @@
 
 function toggleTimer()  {
     var status = "";
-    sessionStorage.SessionName = "jsPOMCLOCK"
+    sessionStorage.SessionName = "jsPOMCLOCK";
     try {
         status = sessionStorage.getItem("jsPOMCLOCKstatus");
     }
@@ -12,14 +12,19 @@ function toggleTimer()  {
         status = "STOP";
         sessionStorage.setItem("jsPOMCLOCKstatus", status);
     }
+    finally {
+        if(status == null) status = "STOP";
+    }
 
     if(status === "STOP") {
-        document.getElementById("titleTimer").innerHTML = "Session count down";
+        document.getElementById("titleTimer").innerHTML = "Session";
+        startTimer();
     }
     else {
-        document.getElementById("titleTimer").innerHTML = "Session";
+        document.getElementById("titleTimer").innerHTML = "Paused";
+        stopTimer();
     }
-}
+};
 
 function howLongChange(minutes) {
     var timespan = document.getElementById("timespan").innerHTML;
@@ -33,4 +38,31 @@ function howLongChange(minutes) {
 
     document.getElementById("timespan").innerHTML = timespan;
     document.getElementById("timeLeft").innerHTML = timespan;
-}
+    stopTimer();
+};
+
+function startTimer() {
+    sessionStorage.setItem("jsPOMCLOCKstatus", "START");
+    var intervallID = setInterval(countDown, 1000);
+    sessionStorage.setItem("jsPOMCLOCKintervallID", intervallID);
+};
+
+function stopTimer() {
+    sessionStorage.setItem("jsPOMCLOCKstatus", "STOP");
+    var intervallID = sessionStorage.getItem("jsPOMCLOCKintervallID");
+    clearInterval(intervallID);
+};
+
+function countDown() {
+    var timeleft = document.getElementById("timeLeft").innerHTML;
+    var timepart = timeleft.split(":");
+    var msectimeleft = new Date(timepart[0]*60000 + timepart[1]*1000 - 1000);
+    if (msectimeleft <= 0) {
+        stopTimer();
+        document.getElementById("timeLeft").innerHTML = "0:00";
+        var audio = new Audio("https://notificationsounds.com/wake-up-tones/good-morning-502/download/mp3");
+        audio.play();
+    } else {
+        document.getElementById("timeLeft").innerHTML = msectimeleft.getMinutes() + ":" + msectimeleft.getSeconds();
+    }
+};
